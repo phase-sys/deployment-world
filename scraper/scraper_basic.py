92f1af1c98ff04bbe43d2deeb7c4f55a9d9033df
Alfred Magat <74297343+phase-sys@users.noreply.github.com>
2022-05-14 09:57:15 +0800

    Add Scraper
    install
    > bs4
    > lxml

---
+++ scraper/scraper_basic.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+
+with open('simple.html') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml')
+    
+for article in soup.find_all('div', class_='article'):
+    headline = article.h2.a.text
+    print(headline)
+
+    summary = article.p.text
+    print(summary)
